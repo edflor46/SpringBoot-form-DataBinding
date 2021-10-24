72bce1b38598e2602f3521347f9ba05faf5b7577
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-10-24 00:26:06 -0500

    Update validation

--- src/main/java/com/edflor/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/edflor/springboot/form/app/controllers/FormController.java
@@ -1,5 +1,21 @@
 package com.edflor.springboot.form.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class FormController {
 
+	@GetMapping("/form")
+	public String formModel(Model model) {
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String procesar(Model model) {
+		return "resultado";
+	}
+	
 }
